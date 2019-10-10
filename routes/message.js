1eb463a77a80c9ea31978f08520ffd6aa5ee770b
Kruglikov Vladislav <outoftender@gmail.com>
2019-10-10 17:03:20 +0300

    Updated

--- routes/message.js
+++ routes/message.js
@@ -1,6 +1,44 @@
 const Router = require('express').Router()
 const userController = require('../controllers/userController')
 
+const io = require('socket.io')();
+
+io.on('connection', (client) => {
+
+    console.log('User connected');
+
+    client.on('joinRoom', (room) => {
+        socket.join(room);
+    })
+
+    client.on('msg', (msg) => {
+        client.to(room).emit('msgEvent', msg);
+    })
+
+    client.on('subscribeToTimer', () => {
+        console.log('emitted subscribeToTimer');
+    });
+
+    io.on('disconnect', function () {
+
+    });
+
+});
+const port = 5000;
+
+io.listen(port);
+
+
+
+
+
+
+
+
+
+
+
+
 Router.post('/addMessage', (req, res, next) => {
     const { usernameFrom, usernameTo, message } = req.body
     userController.addMessage(usernameFrom, usernameTo, message, (err) => {
