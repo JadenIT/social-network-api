80f9f035a9f30db27266dcf2646fb17050c7daf1
Kruglikov Vladislav <outoftender@gmail.com>
2019-10-30 00:14:49 +0300

    Updated

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,8 +1,21 @@
 const router = require('express').Router()
 const jwt = require('jsonwebtoken')
 
+// router.get('/authorize', (req, res, next) => {
+//     const { token } = req.cookies
+
+//     if (!token) return res.send({ isAuthorized: false, token: null })
+//     jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+//         if (err) throw err
+//         res.send({
+//             isAuthorized: decoded,
+//             token: token
+//         })
+//     })
+// })
+
 router.get('/authorize', (req, res, next) => {
-    const { token } = req.cookies
+    const { token } = req.session
 
     if (!token) return res.send({ isAuthorized: false, token: null })
     jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
