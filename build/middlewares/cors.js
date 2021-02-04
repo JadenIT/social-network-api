baf146f12db64ac6fa4492a550e222450e592094
Vladislav Kruglikov <55096567+JadenIT@users.noreply.github.com>
2021-02-04 22:22:24 +0300

    sdfs

--- build/middlewares/cors.js
+++ build/middlewares/cors.js
@@ -1,6 +1,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
+var config_1 = require("../config");
 function cors(req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', config_1.default.CLIENT_URL);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
