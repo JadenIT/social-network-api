25bb03077c9fdb26e2c3211b7da78b60a2497934
Vladislav Kruglikov <55096567+JadenIT@users.noreply.github.com>
2021-02-05 23:54:50 +0300

    sdfdsf

--- build/controllers/GoogleCloud.js
+++ build/controllers/GoogleCloud.js
@@ -2,7 +2,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var storage_1 = require("@google-cloud/storage");
 exports.saveFile = function (content, fileName) {
     var storage = new storage_1.Storage();
-    var myBucket = storage.bucket('example_bucket_16_01');
+    var myBucket = storage.bucket('example_bucket_16_01_2');
     var file = myBucket.file(fileName);
     var contents = content;
     file.save(contents, function (err) {
